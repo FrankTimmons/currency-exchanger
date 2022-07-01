b7885da312ff04a6e762a489e41d22bb82682bc7
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-01 09:43:38 -0700

    Add api call script and functions.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRequest from './exchange-request.js';
+
+function getElements(response){
+  console.log(response);
+}
+
+async function makeApiCall() {
+  let response = await ExchangeRequest.getExchange();
+  getElements(response);
+}
+
+$(document).ready(function(){
+  makeApiCall();
+})
