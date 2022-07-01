896743eec381840138447c893687152ef35e3fc7
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-01 11:46:13 -0700

    Add a function to check if a currency exists.

--- src/exchange-request.js
+++ src/exchange-request.js
@@ -1,7 +1,7 @@
 export default class ExchangeRequest {
   static async getExchange(){
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/US`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error(response.status);   
       }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRequest from './exchange-request.js';
 
-function getElements(response){
+function getElements(response, currency){
+  let currencyExists = checkCurrency(response, currency);
   if (response.result == 'success'){
     $('.errorTest').text(`The call was a ${response.result}`);
   } else {
     $('.errorTest').text(`There was an error: ${response}`);
   }
+  if (currencyExists){
+    console.log('exists');
+  } else {
+    console.log('dont');
+  }
 }
 
-async function makeApiCall() {
+function checkCurrency(response, currency){
+  let currencyExist = false;
+  const keys = Object.keys(response.conversion_rates);
+  keys.forEach((key) => {
+    if (currency == key){
+      currencyExist = true;
+    }
+  });
+  return currencyExist;
+}
+
+async function makeApiCall(currency) {
   const response = await ExchangeRequest.getExchange();
-  getElements(response);
+  getElements(response, currency);
 }
 
 $(document).ready(function(){
-  makeApiCall();
+
+});
+
+$('form#currencyConverter').submit(function(event) {
+  event.preventDefault();
+  let currency = ($('#currency').val()).toUpperCase();
+  makeApiCall(currency);   
 });
